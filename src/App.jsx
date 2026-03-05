8707d7cda024b16f667775aa305d8f546e27f45e
Ulisseswrk <Ulisses.wrk@gmail.com>
2026-03-05 10:40:03 -0300

    add: adiciona as pastas base e instala o react e o tailwind no projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/landing/Hero'
+
+function App() {
+  return (
+    // Definimos o fundo preto em todo o site
+    <main className="min-h-screen bg-black overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/landing/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="relative flex flex-col items-center justify-center min-h-screen px-6 text-center">
+      
+    </section>
+  )
+}
